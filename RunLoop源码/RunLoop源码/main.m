0df160c178b34fcf8fb83da6393f0bf0a58963e1
墨色 <13544004112@163.com>
2020-10-30 11:23:38 +0800

    RunLoop实现线程保活

---
+++ RunLoop源码/RunLoop源码/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  RunLoop源码
+//
+//  Created by admin on 2020/10/28.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
